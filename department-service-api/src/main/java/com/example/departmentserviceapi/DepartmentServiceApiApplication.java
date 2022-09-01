25001d533a7cba3dfb9042215c9e1da077bdf151
邵鑫雨 <2424590457@qq.com>
2022-09-01 20:27:39 +0800

    添加api

---
+++ department-service-api/src/main/java/com/example/departmentserviceapi/DepartmentServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.departmentserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepartmentServiceApiApplication.class, args);
+    }
+
+}
